984e6399343d8a9e201a183be8aade35c02d1ab3
sabahasi1323 <sabahasi1323@gmail.com>
2026-03-29 18:21:26 +0530

    Add Render deployment configuration

--- server.js
+++ server.js
@@ -34,6 +34,8 @@ app.use((err, req, res, next) => {
 
 // Start server
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
+
+// For Render deployment, we need to listen on all interfaces (0.0.0.0)
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`)
 })
